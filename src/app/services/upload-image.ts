698aa61e42c55fb34773bc726e110f70a4a3794d
gabrlcj <gabrielbittencourt57@gmail.com>
2025-03-24 19:31:05 -0300

    refactor: adjusts upload image route and service to use functional error handling

--- src/app/services/upload-image.ts
+++ src/app/services/upload-image.ts
@@ -1,7 +1,9 @@
 import { db } from '@/infra/db'
 import { schema } from '@/infra/db/schemas'
+import { type Either, makeLeft, makeRight } from '@/shared/either'
 import { Readable } from 'node:stream'
 import { z } from 'zod'
+import { InvalidFileFormat } from './errors/invalid-file-format'
 
 const uploadImageInput = z.object({
   fileName: z.string(),
@@ -13,11 +15,13 @@ type UploadImageInput = z.input<typeof uploadImageInput>
 
 const allowedMimeTypes = ['image/jpg', 'image/jpeg', 'image/png', 'image/webp']
 
-export async function uploadImage(input: UploadImageInput) {
+export async function uploadImage(
+  input: UploadImageInput
+): Promise<Either<InvalidFileFormat, { url: string }>> {
   const { fileName, contentType, contentStream } = uploadImageInput.parse(input)
 
   if (!allowedMimeTypes.includes(contentType)) {
-    throw new Error('Invalid file format.')
+    return makeLeft(new InvalidFileFormat())
   }
 
   // TODO: enviar arquivos para o cloudflare R2
@@ -27,4 +31,6 @@ export async function uploadImage(input: UploadImageInput) {
     redirectUrl: fileName,
     remoteKey: fileName,
   })
+
+  return makeRight({ url: '' })
 }
