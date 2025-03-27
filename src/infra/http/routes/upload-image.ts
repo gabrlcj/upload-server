f4abec7d85651a60793869921dd2dc6e787339dd
gabrlcj <gabrielbittencourt57@gmail.com>
2025-03-27 18:23:11 -0300

    chore: adds tags to the route and removes console.log

--- src/infra/http/routes/upload-image.ts
+++ src/infra/http/routes/upload-image.ts
@@ -9,6 +9,7 @@ export const uploadImageRoute: FastifyPluginAsyncZod = async server => {
     {
       schema: {
         summary: 'Upload an image',
+        tags: ['uploads'],
         consumes: ['multipart/form-data'],
         response: {
           201: z.null().describe('Image uploaded'),
@@ -40,8 +41,6 @@ export const uploadImageRoute: FastifyPluginAsyncZod = async server => {
       }
 
       if (isRight(result)) {
-        console.log(unwrapEither(result))
-
         return reply.status(201).send()
       }
 
